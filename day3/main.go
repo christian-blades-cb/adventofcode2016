dd35b618e9aa250c99491280d2a5270c0af119e9
Christian Blades <cblades@pindrop.com>
2016-12-09 00:05:53 -0500

    day3 pt 2

--- day3/main.go
+++ day3/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strconv"
 )
@@ -28,8 +29,78 @@ import (
 // In your puzzle input, how many of the listed triangles are possible?	  //
 ////////////////////////////////////////////////////////////////////////////
 
+///////////////////////////////////////////////////////////////////////////
+// --- Part Two ---														 //
+// 																		 //
+// Now that you've helpfully marked up their design documents, it occurs //
+// to you that triangles are specified in groups of three				 //
+// vertically. Each set of three numbers in a column specifies a		 //
+// triangle. Rows are unrelated.										 //
+// 																		 //
+// For example, given the following specification, numbers with the same //
+// hundreds digit would be part of the same triangle:					 //
+// 																		 //
+// 101 301 501															 //
+// 102 302 502															 //
+// 103 303 503															 //
+// 201 401 601															 //
+// 202 402 602															 //
+// 203 403 603															 //
+// 																		 //
+// In your puzzle input, and instead reading by columns, how many of the //
+// listed triangles are possible?										 //
+///////////////////////////////////////////////////////////////////////////
+
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+	fd, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer fd.Close()
+
+	fmt.Println(numValidRows(fd))
+	fd.Seek(0, 0)
+	fmt.Println(numValidCols(fd))
+}
+
+func numValidCols(in io.Reader) int {
+	vals := readColumnarVals(in)
+	var validTriangles int
+	var triangle [3]int
+	for i, x := range vals {
+		triangle[i%3] = x
+		if i%3 == 2 {
+			a, b, c := triangle[0], triangle[1], triangle[2]
+			if a+b > c && a+c > b && b+c > a {
+				validTriangles++
+			}
+		}
+	}
+	return validTriangles
+}
+
+func readColumnarVals(in io.Reader) []int {
+	scanner := bufio.NewScanner(in)
+	scanner.Split(bufio.ScanWords)
+	columnar := [3][]int{[]int{}, []int{}, []int{}}
+	i := 0
+	for scanner.Scan() {
+		x, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			panic(err)
+		}
+		columnar[i] = append(columnar[i], x)
+		if i == 2 {
+			i = 0
+		} else {
+			i++
+		}
+	}
+	return append(append(columnar[0], columnar[1]...), columnar[2]...)
+}
+
+func numValidRows(in io.Reader) int {
+	scanner := bufio.NewScanner(in)
 	scanner.Split(bufio.ScanWords)
 	var err error
 	var validTriangles int
@@ -51,5 +122,5 @@ func main() {
 			i++
 		}
 	}
-	fmt.Println(validTriangles)
+	return validTriangles
 }
